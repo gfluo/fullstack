3c22f703559a0ba702eeeca2ef55a2aa2d4ff1e5
luogf <zoroshow@outlook.com>
2017-11-24 11:10:07 +0800

    add api /users/home

--- serve/routes/users.js
+++ serve/routes/users.js
@@ -1,13 +1,18 @@
-const router = require('koa-router')()
+'use strict';
+const router = require('koa-router')();
 
-router.prefix('/users')
+router.prefix('/users');
 
 router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
-})
+});
 
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
-})
+});
 
-module.exports = router
+router.get('home', (ctx, next) =>  {
+  ctx.body = 'welcome';
+});
+
+module.exports = router;
